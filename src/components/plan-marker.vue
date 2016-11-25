379dfeef7c8fdb0c6eb713ed963c161f665e2afb
Perajit Pasupat <perajit.pasupat@thomsonreuters.com>
2016-11-25 18:35:18 +0700

    Add plan-marker

---
+++ src/components/plan-marker.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <canvas ref="canvas" :width="width" :height="height"></canvas>
+  </div>
+</template>
+
+<script>
+import fabric from 'fabric';
+
+export default {
+  data() {
+    return {
+      imgSrc: null,
+      width: 800,
+      height: 600,
+      fabricCanvas: null
+    };
+  },
+  watch: {
+    imgSrc: 'imgSrcObserver',
+  },
+  methods: {
+    initComponents() {
+      var fabricCanvas = new fabric.Canvas(this.$refs.canvas);
+      fabricCanvas.on('mouse:down', this.onCanvasMouseDown);
+
+      this.fabricCanvas = fabricCanvas;
+    },
+    imgSrcObserver: function (val, oldVal) {
+      var fabricCanvas = this.fabricCanvas;
+      fabricCanvas.setBackgroundImage(val, fabricCanvas.renderAll.bind(fabricCanvas), {
+        originX: 'left',
+        originY: 'top'
+      });
+    },
+    onCanvasMouseDown: function (event) {
+      var fabricCanvas = this.fabricCanvas;
+
+      // Create desk mark rect only when there is no selected object
+      // Let Fabric handle object manipulation
+      if (fabricCanvas.getActiveObject()) {
+        return;
+      }
+
+      var pointer = fabricCanvas.getPointer(event.e);
+      var posX = pointer.x;
+      var posY = pointer.y;
+
+      this.addDeskMarkRect(this.createDeskMarkRect(posX, posY));
+    },
+    createDeskMarkRect: function (posX, posY) {
+      // Create desk mark
+      var rect = new fabric.Rect({
+        left: posX,
+        top: posY,
+        width: 100,
+        height: 100,
+        fill: 'red',
+      });
+      return rect;
+    },
+    addDeskMarkRect: function (deskMarkRect) {
+      this.fabricCanvas.add(deskMarkRect);
+    },
+  },
+  mounted() {
+    this.initComponents();
+  },
+};
+</script>
+
+<style scoped>
+</style>
