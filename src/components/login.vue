b0ab72bf7c2d0a0094c31dde239c4f3fe63d110a
Isara Rungvitayakul <isaru_66@hotmail.com>
2016-11-25 14:34:39 +0700

    add bulma css & change login page

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,23 @@
 <template lang="html">
-  <div>
-    This is login page
+  <div class="column is-4 is-offset-4">
+    <h1 class="title">
+      FPV Vue Login
+    </h1>
+    <div class="box">
+      <label class="label">Email</label>
+      <p class="control">
+        <input class="input" type="text" placeholder="username">
+      </p>
+      <label class="label">Password</label>
+      <p class="control">
+        <input class="input" type="password" placeholder="password">
+      </p>
+      <hr>
+      <p class="control">
+        <button class="button is-primary">Login</button>
+        <button class="button is-default">Cancel</button>
+      </p>
+    </div>
   </div>
 </template>
 
