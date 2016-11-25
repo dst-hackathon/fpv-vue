9d8b590622d8a51d963746b352fc5f620090937e
Kittisak Wongkraphan <kimgun218@gmail.com>
2016-11-25 15:20:52 +0700

    Update Menu in home page and add more components (view, create, approve, and manage)

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,53 @@
 <template lang="html">
-  <div>
-    This is home page
-  </div>
+    <div id="home">
+        <h1>{{leftNavTitle}}</h1>
+        <div v-for="menu in menuList">
+            <a href="#">{{menu}}</a>
+        </div>
+
+        <!-- Page Content -->
+        <div v-if="menuSelected === 'create'">
+            <create></create>
+        </div>
+        <div v-else-if="menuSelected === 'approve'">
+            <approve></approve>
+        </div>
+        <div v-else-if="menuSelected === 'manage'">
+            <manage></manage>
+        </div>
+        <div v-else>
+            <ViewPage></ViewPage>
+        </div>
+    </div>
 </template>
 
 <script>
+import ViewPage from './view';
+import Create from './create';
+import Approve from './approve';
+import Manage from './manage';
+
 export default {
+  name: 'app',
+  components: {
+    ViewPage,
+    Create,
+    Approve,
+    Manage,
+  },
+  data() {
+    return {
+      menuSelected: '',
+      leftNavTitle: 'Menu',
+      menuList: [
+        'ViewPage Master Plan',
+        'Create/Edit Future Plan',
+        'Approve Future Plan',
+        'Manage Floor Plan',
+        'Log out',
+      ],
+    };
+  },
 };
 </script>
 
