5b9711e41435b5a1fb6d856b249701f5fb4d5394
smileyOpal <opal@kitchenup.com>
2016-11-26 15:19:10 +0700

    Merge branch 'master' of https://github.com/dst-hackathon/fpv-vue
    # Conflicts:
    #	src/components/home/canvas/desk.vue

--- src/components/home/canvas/desk.vue
+++ src/components/home/canvas/desk.vue
@@ -95,13 +95,13 @@ export default {
       ...this.position
     });
 
-        var person = new fabric.Image.fromURL('/static/img/location.png', (img) => {
+    var person = new fabric.Image.fromURL('/static/img/location.png', (img) => {
       img.setLeft(this.position.left + this.dimensions.width/2 - 25);
       img.setTop(this.position.top - this.dimensions.height/2);
       img.setWidth(50);
       img.setHeight(50);
-    var group = new fabric.Group([this.deskShape, img]);
-    this.$emit('created', { shape: group });
+      var group = new fabric.Group([this.deskShape, img]);
+      this.$emit('created', { shape: group });
     });
 
 
@@ -111,7 +111,6 @@ export default {
       this.lockRotation();
     }
 
-    this.$emit('created', { shape: this.deskShape });
   },
 
   destroyed() {
