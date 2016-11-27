873e934066fc8bc6aef1d088aa57a9c52236ca9d
starkhorn <t.earendel@gmail.com>
2016-11-27 19:29:09 +0700

    View master plan page.

--- src/components/home/canvas/desk.vue
+++ src/components/home/canvas/desk.vue
@@ -181,7 +181,6 @@ export default {
   },
 
   mounted() {
-    console.log('mounted');
     this.$nextTick(() => {
       this.updateEmployeeTag();
     });
--- src/components/home/view-master-plan.vue
+++ src/components/home/view-master-plan.vue
@@ -1,33 +1,98 @@
 <template lang="html">
-  <div>
+  <div :style="style.main">
     <div class="toolbar">
       <nav class="level">
         <!-- Left Side -->
         <div class="level-left">
-
         </div>
 
         <!-- Right Side -->
         <div class="level-right">
-
+          <FloorPlanSelector class="level-right" @selected="selectedFloor = $event.floor" />
         </div>
       </nav>
     </div>
 
-    <floor-canvas />
+    <floor-canvas
+      :readOnly="true"
+      :floor="selectedFloor"
+      :showEmployee="true"
+      @deskSelected="selectedDesk = $event.desk"
+      @deskDeselected="selectedDesk = null"/>
+
+    <detail-panel :width="detailWidth" v-show="selectedDesk">
+      <desk-detail-panel :desk="selectedDesk" :fieldOptions="deskFieldOptions" />
+    </detail-panel>
   </div>
 </template>
 
 <script>
-import * as types from 'store/types';
 import FloorCanvas from './canvas/floor-canvas';
+import DetailPanel from './detail-panel';
+import DeskDetailPanel from './desk-detail-panel';
+import Datepicker from 'vue-bulma-datepicker';
+import FloorPlanSelector from './floor-plan-selector';
 
 export default {
+
+  data() {
+    return {
+      selectedFloor: null,
+      selectedDesk: null,
+    };
+  },
+
   components: {
     FloorCanvas,
+    DetailPanel,
+    DeskDetailPanel,
+    Datepicker,
+    FloorPlanSelector,
   },
+
+  computed: {
+    style() {
+      return {
+        main: {
+          'margin-right': `${this.detailWidth}px`
+        }
+      };
+    },
+
+    detailWidth() {
+      if (this.selectedDesk) {
+        return 300;
+      } else {
+        return 0;
+      }
+    },
+
+    deskFieldOptions() {
+      return {
+        deskCode: { readonly: true },
+        employeeId: { readonly: false },
+        firstName: { readonly: true },
+        lastName: { readonly: true },
+      };
+    },
+  },
+
+  watch: {
+    ['selectedFloor.id'](floorId) {
+      if (!floorId) {
+        return;
+      }
+
+      this.$store.dispatch('FETCH_DESK_ASSIGNMENTS', {
+        floorId
+      });
+    }
+  }
 };
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+  .toolbar {
+    padding: 10px;
+  }
 </style>
