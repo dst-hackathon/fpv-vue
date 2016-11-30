35d141d11b61f86c4fc29fbb3e2dd4b1d389493e
starkhorn <t.earendel@gmail.com>
2016-11-30 08:30:31 +0700

    Create template for owner assigning.

--- src/components/home/desk-assignment-panel.vue
+++ src/components/home/desk-assignment-panel.vue
@@ -5,16 +5,34 @@
     <figure class="image-wrapper">
       <img class="owner-image" :src="ownerImage">
 
-      <div v-if="owner">
-        <h4 class="owner-title title is-4">{{ owner.firstname }} {{ owner.lastname }}</h4>
-        <h5 class="owner-subtitle subtitle is-5">
-          <span class="icon is-small"><i class="fa fa-phone"></span></i> {{ owner.work }}
-        </h5>
+      <div v-if="isEditingOwner">
+        <p class="owner-query title is-4" placeholder="Enter name ..." contenteditable="true"></p>
+        <footer class="card-footer owner-action">
+          <span class="card-footer-item" v-show="selectedOwner">
+            <a>Save</a>
+          </span>
+          <span class="card-footer-item">
+            <a class="remove">Cancel</a>
+          </span>
+        </footer>
       </div>
-      <footer class="card-footer owner-action" v-if="editable">
-        <a class="card-footer-item">{{ owner ? 'Change' : 'Assign Owner' }}</a>
-        <a class="card-footer-item remove" v-show="owner" @click="removeOwner">Remove</a>
-      </footer>
+      <div v-else>
+        <div v-if="owner">
+          <h4 class="owner-title title is-4">{{ owner.firstname }} {{ owner.lastname }}</h4>
+          <h5 class="owner-subtitle subtitle is-5">
+            <span class="icon is-small"><i class="fa fa-phone"></span></i> {{ owner.work }}
+          </h5>
+        </div>
+        <footer class="card-footer owner-action" v-if="editable">
+          <span class="card-footer-item" @click="editOwner">
+            <a>{{ owner ? 'Change' : 'Assign Owner' }}</a>
+          </span>
+          <span class="card-footer-item" v-show="owner" @click="removeOwner">
+            <a class="remove">Remove</a>
+          </span>
+        </footer>
+      </div>
+
     </figure>
   </div>
 </template>
@@ -22,6 +40,13 @@
 <script>
   export default {
     props: ['desk', 'editable'],
+
+    data() {
+      return {
+        isEditingOwner: false,
+        selectedOwner: null,
+      };
+    },
 
     computed: {
       owner() {
@@ -39,10 +64,24 @@
       }
     },
 
+    watch: {
+      desk() {
+        this.isEditingOwner = false;
+      }
+    },
+
     methods: {
       removeOwner() {
         this.$emit('removeOwner', {
           desk: this.desk
+        });
+      },
+
+      editOwner() {
+        this.isEditingOwner = true;
+
+        this.$nextTick().then(() => {
+          this.$el.querySelector('.owner-query').focus();
         });
       }
     }
@@ -51,10 +90,9 @@
 
 <style lang="scss" scoped>
   @import '~bulma/sass/utilities/variables';
-
   .image-wrapper {
     text-align: center;
-    border: 1px solid #CCC;
+    border: 1px solid $grey-lighter;
   }
 
   .owner-image {
@@ -74,5 +112,26 @@
 
   .owner-action .remove:not(:hover) {
     color: $red;
+  }
+
+  .owner-action .card-footer-item {
+    cursor: pointer;
+
+    &:hover > a {
+      color: $grey;
+    }
+  }
+
+  .owner-query:empty:before {
+    content: attr(placeholder);
+    font-style: italic;
+    color: $grey-lighter;
+  }
+
+  .owner-query {
+    text-decoration: none;
+    margin-left: 10px;
+    margin-right: 10px;
+    outline: none;
   }
 </style>
